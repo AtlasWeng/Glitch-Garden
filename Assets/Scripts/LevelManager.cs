1bd09879c394d5abf2bfe1cc70f379d8069edfcb
Ata Weng <gongziy@foxmail.com>
2017-12-22 12:22:30 +0000

    Initial commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+	public float intervalTime = 2f;
+
+	void OnEnable () {
+		SceneManager.sceneLoaded += OnSceneLoaded;
+	}
+
+	void OnSceneLoaded (Scene scene, LoadSceneMode mode)
+	{
+		if (scene.buildIndex == 0) {
+			AutoLoadNextScene();
+		}
+	}
+
+	void OnDisable () {
+		SceneManager.sceneLoaded -= OnSceneLoaded;
+	}
+
+	public void LoadScene (string level) {
+		SceneManager.LoadScene(level);
+	}
+
+	public void LoadNextScene () {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	void AutoLoadNextScene () {
+		Invoke("LoadNextScene", intervalTime);
+	}	
+}
---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+	public AudioClip[] levelMusicChangeArray;
+	private AudioSource _audio;
+
+	void Awake ()
+	{
+		DontDestroyOnLoad(gameObject);
+		_audio = GetComponent<AudioSource>()	;
+	}
+
+	void OnEnable ()
+	{
+		SceneManager.sceneLoaded += OnSceneLoaded;
+	}
+
+	void OnSceneLoaded (Scene scene, LoadSceneMode mode)
+	{	
+		AudioClip thisLevelMusic = levelMusicChangeArray[scene.buildIndex];
+
+		if (thisLevelMusic) {
+			_audio.clip = levelMusicChangeArray[scene.buildIndex];
+			_audio.loop = true;
+			_audio.Play();
+		}
+
+	}
+
+	void OnDisable () 
+	{
+		SceneManager.sceneLoaded -= OnSceneLoaded;
+	}
+	
+}
